58b8439d262704545efe78eb9e8626514ceb8813
rumirumi18 <77532665+rumirumi18@users.noreply.github.com>
2025-01-02 01:25:48 +0900

    Fix issue and prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Services from './components/Services';
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home';  // 修正されたインポートパス
+import Services from './Services';  // 修正されたインポートパス
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
-        {/* ヘッダー部分 */}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-
-        {/* React Router を使ったページのルーティング */}
+      <div>
+        {/* ルーティング設定 */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
